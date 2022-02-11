549548701984802cad01d1f0850a769605c3eaae
wicaksop <pradityo.wicaksono@etu.univ-grenoble-alpes.fr>
2022-02-11 09:27:59 +0100

    automated data cleaning

--- scrape.py
+++ scrape.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.service import Service
 
 
 #initialisation
-ser = Service("BuDjoko-Scraper\chromedriver_win32\chromedriver.exe")
+ser = Service("chromedriver_win32\chromedriver.exe")
 op = webdriver.ChromeOptions()
 
 # disable all the errors getting displayed on Console
@@ -20,7 +20,7 @@ prices=[]
 productCode=[]
 itemAvailability=[]
 poids = []
-
+form=[]
 
 
 driver = webdriver.Chrome(service=ser, options=op)
@@ -28,28 +28,49 @@ driver = webdriver.Chrome(service=ser, options=op)
 # pages to parse
 pagelinks = ["https://budjoko.fr/fr/epicerie-fine", "https://budjoko.fr/fr/boissons", "https://budjoko.fr/fr/sante-et-soins", "https://budjoko.fr/fr/non-alimentaire"]
 
+
 #opening those pages
-for i in pagelinks:
-    print("parsing "+ i)
+for page in pagelinks:
+    print("parsing "+ page)
 
     ##opening page i
-    driver.get(i)
+    driver.get(page)
 
     # scrolling and waiting because it takes time for the page to load (javascript de merde)
     ScrollNumber=5
     for i in range(1, ScrollNumber):
-        time.sleep(5)
+        time.sleep(3)
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     
     content = driver.page_source
     soup = BeautifulSoup(content, features="html.parser")
         
     for e in soup.find_all('div', {"class":"hkc-md-2"}):
+        # append the product's hyperlink
         href.append(e.find('a')['href'])
+
+        # append the product's title
         products.append(e.find('a')['title'])
-        prices.append(e.find('span', {"class":"hikashop_product_price"}).text)
+
+        # If the price isn't discounted then append full price.
+        # replace comma with point and then convert text to float
+        if(e.find('span', {"class":"hikashop_product_price_with_discount"}) == None):
+            prices.append(float(e.find('span', {"class":"hikashop_product_price"}).text.replace('€','').replace(' ','').replace(',','.')))
+        else:
+            prices.append(float(e.find('span', {"class":"hikashop_product_price_with_discount"}).text.replace('€','').replace(' ','').replace(',','.')))
+        
+        # append the product's code, eliminate whitespaces
         productCode.append(e.find('span', {'class':'hikashop_product_code_list'}).find('a').text.replace('\n','').replace('\t',''))
+
+        # append the product's availability (add "Stock épuisé" if no more stock)
         itemAvailability.append(e.find('span', {'class':'hikashop_product_stock_count'}).text)
+        
+        # append add to cart hyperlink (will be used to execute orders)
+        linkToCart = e.find('a',{'class':'hikabtn hikacart'})
+        if(linkToCart==None):
+            form.append("not available")
+        else:
+            form.append(linkToCart['href'])
 
 
 ## scraping weight of each item
@@ -68,17 +89,23 @@ for item in href:
     weight = itemPageSoup.find('span', {'class':'hikashop_product_weight_main'})
     # to avoid AttributeError: 'NoneType' object has no attribute 'text'
     if(weight != None):
-        poids.append(weight.text)
+        weight = weight.text.replace('\n','').replace('Poids brut: ','')
+        if(weight.__contains__("kg")):
+            weight = weight.replace('kg','')
+            weight = float(weight) * 1000
+        else:
+            weight = weight.replace('g','')
+        poids.append(float(weight))
     else:
         poids.append("-1")
 
 
 #creating a data frame and export to excel
 ## creating data frame based on created list
-df = pd.DataFrame(list(zip(products, prices, productCode, itemAvailability, poids, href)),columns=["product name", "price", "product code", "available", "weight", "href"])
+df = pd.DataFrame(list(zip(products, prices, productCode, itemAvailability, poids, href, form)),columns=["product name", "price", "product code", "available", "weight", "href", "form link"])
 
 ## exporting to excel
 df.to_excel("scraped.xlsx")
 
 print("end of the program")
-
+driver.close()
